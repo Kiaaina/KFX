5a085e055dffdcae84583e745244023b416a577c
Kiaaina JA Medeiros <161550532+Kiaaina@users.noreply.github.com>
2024-02-27 20:19:35 -1000

    Add files via upload

---
+++ link.cmd
@@ -0,0 +1,11 @@
+cd /
+:top
+cls
+@cecho off
+title "dropbox"
+@mod con: lines=5 cols=20
+
+start https://www.dropbox.com/scl/fo/ht5is6w5o0tlg6rgoaui0/h?rlkey=xmnm23yo5qies9szettsoil50&dl=0
+cecho {09}Done{/h}
+timeout /t 5
+exit
