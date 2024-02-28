4ee1757a7585ddea1e1193211489b3ac84469289
Kiaaina JA Medeiros <161550532+Kiaaina@users.noreply.github.com>
2024-02-28 13:00:57 -1000

    Update link.cmd

--- link.cmd
+++ link.cmd
@@ -5,7 +5,7 @@ cls
 title "dropbox"
 @mod con: lines=5 cols=20
 
-start https://www.dropbox.com/scl/fo/ht5is6w5o0tlg6rgoaui0/h?rlkey=xmnm23yo5qies9szettsoil50&dl=0
+start https://github.com/Kiaaina/Sweeter_fx.git
 cecho {09}Done{/h}
 timeout /t 5
 exit
